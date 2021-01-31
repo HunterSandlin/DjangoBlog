785a7413cf17cb1cd32a8e65d716a75146c0fd7f
Hunter Sandlin <hsandlin@yahoo.com>
2021-01-31 16:18:25 -0500

    Set up articles page in articles app

--- djangoblog/urls.py
+++ djangoblog/urls.py
@@ -1,9 +1,11 @@
+from django.urls import path, include
 from django.contrib import admin
-from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.home),
+    path('articles/', include('articles.urls')),
     path('admin/', admin.site.urls),
     path('about/', views.about),
-    path('', views.home)
 ]
+ 
